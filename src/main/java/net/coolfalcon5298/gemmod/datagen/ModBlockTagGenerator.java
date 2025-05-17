652d08f8bf792b3225ef64bdf20dc1a408de4c47
Hacker-Ethical1 <edwardkbellis@gmail.com>
2025-05-17 11:52:42 -0700

    added Stairs, Fences, Walls, Buttons, ect.

--- src/main/java/net/coolfalcon5298/gemmod/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/coolfalcon5298/gemmod/datagen/ModBlockTagGenerator.java
@@ -31,5 +31,12 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                 .add(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(), ModBlocks.END_STONE_SAPPHIRE_ORE.get());
         //this.tag(Tags.Blocks.NEEDS_NETHERITE_TOOL).add()
 
+        this.tag(BlockTags.FENCES)
+                .add(ModBlocks.SAPPHIRE_FENCE.get());
+        this.tag(BlockTags.FENCE_GATES)
+                .add(ModBlocks.SAPPHIRE_FENCE_GATE.get());
+        this.tag(BlockTags.WALLS)
+                .add(ModBlocks.SAPPHIRE_WALL.get());
+
     }
 }
